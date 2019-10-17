62eea014b07630ccf296f707879ddbdab245e4b9
k-lord <katielord27@gmail.com>
2019-10-17 15:36:49 -0400

    worked on css for <p> spacing on gif ratings but still not fixed

--- assets/gif.js
+++ assets/gif.js
@@ -6,15 +6,20 @@ var createButton = function (arr) {
     for(i = 0; i < arr.length; i ++) {
         var searchWord = arr[i];
         console.log(searchWord);
+        var button = $("<button>").text(arr[i]);
+        button.addClass("btn btn-dark");
+        
+        
 
         //append button to HTML page for each index of array
 
-        $("#btn-placement").append("<button class='btn btn-dark' id='gifbtn'>");
+        $("#btn-placement").append(button);
         
         //add data-search to each button that = the array index
+       
 
-        $("#gifbtn").data("search", searchWord);
-        var x = $("#gifbtn").data("search");
+        button.data("search", searchWord);
+        var x = button.data("search");
         console.log("data search is " + x);
 
         //add text to each button that = the array index
@@ -26,7 +31,7 @@ var createButton = function (arr) {
 
 createButton(topics);
 
-// on click function to GET gifs from Giphy based on data-search id's & prepend to HTML
+// on click function to GET gifs from Giphy based on data-search id's & prepend response.data[i] info to HTML
 
 
 $("button").on("click", function(){
@@ -39,9 +44,11 @@ $("button").on("click", function(){
     $.ajax({url: queryURL, method: "GET"})
         .done(function(response){
             console.log(response);
+            $("#GIF-area").empty();
             for (i = 0; i < response.data.length; i++) {
-                $("#GIF-area").prepend("<p>Rating: " + response.data[i].rating + "</p>");
-                $("#GIF-area").prepend("<img src='" + response.data[i].images.downsized.url + "'>");
+            
+                $("#GIF-area").append("<img src='" + response.data[i].images.downsized.url + "'>");
+                $("#GIF-area").append("<p>rating: " + response.data[i].rating + "</p>");
     
 
 
@@ -50,6 +57,11 @@ $("button").on("click", function(){
 })
 
 
-
+/*<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>*/
 
 
