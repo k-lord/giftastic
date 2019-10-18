9507af7e0441d9223f1d5847f232cdd669755e92
k-lord <katielord27@gmail.com>
2019-10-18 12:36:25 -0400

    added a function to toggle between still imgs and playing gifs in js and updated widths of #GIF-area in CSS

--- assets/gif.js
+++ assets/gif.js
@@ -1,7 +1,10 @@
-var topics = ["excited","angry","happy","hangry","surprised","sad","wild"]; 
+// the array of gif button topics
+var topics = ["excited","angry","happy","hangry","surprised","sad","wild"];
 
-// a function to run through a for loop of the array and create buttons for each index
+//boolean statement to call on later on to toggle the GIFs between paused and playing
+var gifPaused = true;
 
+// the function to run through a for loop of the topics array and create buttons for each index
 var createButton = function (arr) {
 
     $("#btn-placement").empty();
@@ -12,28 +15,21 @@ var createButton = function (arr) {
         var button = $("<button>").text(arr[i]);
         button.addClass("btn btn-dark gifs");
         
-        
-
         //append button to HTML page for each index of array
-
         $("#btn-placement").append(button);
         
         //add data-search to each button that = the array index
-       
-
         button.data("search", searchWord);
         var x = button.data("search");
         console.log("data search is " + x);
 
         //add text to each button that = the array index
-
         $("#gifbtn").text(searchWord);
         console.log("button text is " + searchWord);
     }
 }
 
-// on click function to GET gifs from Giphy based on data-search id's & prepend response.data[i] info to HTML
-
+// the on-click function to .ajax(GET) gifs from Giphy based on data-search id's & prepend response.data[i] info to HTML
 function getGIF () {
 
     $("button").on("click", function(){
@@ -41,26 +37,26 @@ function getGIF () {
         var x = $(this).data("search");
         console.log(x);
     
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=Yto3qlUaI5924crOZKDBgZeRIvCm4xgz&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=Yto3qlUaI5924crOZKDBgZeRIvCm4xgz&limit=12";
         console.log(queryURL);
     
         $.ajax({url: queryURL, method: "GET"})
             .done(function(response){
                 console.log(response);
-                $("#GIF-area").empty();
+                $("#GIF-area").empty(); 
                 for (i = 0; i < response.data.length; i++) {
                 
-                    $("#GIF-area").append("<img src='" + response.data[i].images.downsized.url + "'>");
-                    //$("#GIF-area").append("<p>rating: " + response.data[i].rating + "</p>");   
+                    $("#GIF-area").append("<img class='gifImage' src='" + response.data[i].images.fixed_height_still.url + "'>");
+                    
+                    //$("#GIF-area").append("<p>rating: " + response.data[i].rating + "</p>");
                 }
+
+
             });
     })   
 }
 
-
-
-// take the value of the #add-GIF input text and add to topics array & create button
-
+// take the value of the #add-GIF input text from HTML, add to topics array & add the new button to HTML
 $("#add-GIF").on("click", function(event) {
     event.preventDefault();
     var GIF = $("#GIF-input").val().trim();
@@ -69,8 +65,26 @@ $("#add-GIF").on("click", function(event) {
     $("#GIF-input").val("");
 });
 
-// Adding click event listeners to all elements with a class of "gifs"
+//function to toggle between paused imgs and playing gifs
+
+$(document).on("click", ".gifImage", function() {
+    var src = $(this).attr("src");
+    if($(this).hasClass('playing')){
+     //stop
+        $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
+        $(this).removeClass('playing');
+    } else {
+    //play
+    $(this).addClass('playing');
+    $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
+  }
+});
+
+
+
+// Adding click event listeners to all elements with a class of ".gifs"
 $(document).on("click", ".gifs", getGIF);
+
 
 createButton(topics);
 
